992b64044969f79fe7fa042a16d37d61888db3cb
Mike Clancy <mclancy96@gmail.com>
2025-02-19 21:41:46 -0500

    add left and right move functions

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+const moveDodgerLeft = () => {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+const moveDodgerRight = () => {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
